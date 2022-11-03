3af0e413f80bd3c6e0e12f20303129bcfc80a994
Nuria Rueda <ruedareinanuria@gmail.com>
2022-11-03 09:48:50 +0100

    added

---
+++ Ejercicio20.java
@@ -0,0 +1,31 @@
+public class Ejercicio20 { 
+    public static void main(String[] args) {
+        System.out.print("Introduzca la altura de la pirámide: ");
+        int alturaIntroducida = Integer.parseInt(System.console().readLine());
+        System.out.print("Introduzca con que lo quieres rellenar: ");
+        String relleno = System.console().readLine();
+        int altura = 1;
+        int i = 0;
+        int espaciosPorDelante = alturaIntroducida - 1;
+        int espaciosInternos = 0;
+        while (altura < alturaIntroducida) {
+            for (i = 1; i <= espaciosPorDelante; i++) {
+            System.out.print(" ");
+            }
+            System.out.print(relleno);
+            for (i = 1; i < espaciosInternos; i++) {
+            System.out.print(" ");
+            }
+            if (altura>1) {
+            System.out.print(relleno);
+            }
+            System.out.println();
+            altura++;
+            espaciosPorDelante--;
+            espaciosInternos += 2;
+        } 
+        for (i = 1; i < altura*2; i++) {
+            System.out.print(relleno);
+        }
+    }
+}
---
+++ Ejercicio22.java
@@ -0,0 +1,18 @@
+public class Ejercicio22 {
+    public static void main (String[]args){
+        System.out.println ("Este programa te muestra los numero primos que hay entre 2 y 100: ");
+        boolean primo = true;
+        for (int n = 2; n <= 100; n++){
+            primo = true;
+            for (int i = 2; i < n; i++){
+                if (n % i == 0 ){
+                    primo = false;
+                }
+            }
+            if (primo) {
+                System.out.print(n + " ");
+            }
+        }
+        System.out.println();
+    }
+}
