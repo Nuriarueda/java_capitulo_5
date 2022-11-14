305653ac50337544e9e00b2923d500ab05cea71b
Nuria Rueda <ruedareinanuria@gmail.com>
2022-11-14 08:47:45 +0100

    corregidos

--- Ejercicio69.java
+++ Ejercicio69.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 public class Ejercicio69 {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce la altura de la pirámide maya: ");
+        int tam = sc.nextInt();
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Introduzca la altura de la pirámide maya: ");
-        int alturaIntroducida = Integer.parseInt(s.nextLine());
-        int planta = 1;
-        int longitudDeLinea = 1;
-        int espacios = alturaIntroducida - 1;
-        
-        while (planta <= alturaIntroducida) {
-            for (int i = 1; i <= espacios; i++) {
-                System.out.print(' ');
+
+        for (int i=1; i<=tam; i++) {
+            for (int j=1; j<=tam-i; j++) {
+                System.out.print(" ");
             }
-            for (int i = 0; i <= longitudDeLinea / 2; i++) {
-                System.out.print('*');
+            for (int j=1; j<=3; j++) {
+                if (j == 1 || j == 3) {
+                    for (int k=1; k<=i; k++) { 
+                        System.out.print("*");
+                    }
+                } else  {
+                    if (i%2==0) { 
+                        System.out.print("    ");
+                    } else {
+                        System.out.print("****");
+                    }
+                }
             }
-            System.out.print(planta % 2 == 1 ? "****" : " ");
-            for (int i = 0; i <= longitudDeLinea / 2; i++) {
-                System.out.print('*');
-            }
-            System.out.println();
-            planta++;
-            espacios--;
-            longitudDeLinea += 2;
+            System.out.println("");
         }
-        s.close();
+        sc.close();
     }
 }
